1adc9d4dbcf0f6baea46efa0ab98c299164f8564
Microsoft Open Source <microsoftopensource@users.noreply.github.com>
2024-01-18 12:48:16 -0800

    Add repository structure for gitops bridge pattern

---
+++ terraform/provider.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.56"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.10.1"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+resource "local_file" "kubeconfig" {
+  content  = module.aks.kube_config_raw
+  filename = "${path.module}/kubeconfig"
+}
+
+provider "kubernetes" {
+  config_path = local_file.kubeconfig.filename
+
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = local_file.kubeconfig.filename
+  }
+
+}
+provider "random" {}
---
+++ terraform/variables.tf
@@ -0,0 +1,83 @@
+variable "region" {
+  type    = string
+  default = "eastus"
+}
+
+variable "agents_size" {
+  default     = "Standard_D8s_v3"
+  description = "The default virtual machine size for the Kubernetes agents"
+  type        = string
+}
+
+variable "kubernetes_version" {
+  description = "Specify which Kubernetes release to use. The default used is the latest Kubernetes version available in the region"
+  type        = string
+  default     = null
+}
+
+variable "addons" {
+  description = "Kubernetes addons"
+  type        = any
+  default = {
+    enable_aws_load_balancer_controller = false
+    enable_metrics_server               = false
+    # Enable argocd with IRSA
+    enable_aws_argocd = false
+    # Disable argocd without IRSA
+    enable_argocd = true
+  }
+}
+
+# Addons Git
+variable "gitops_addons_org" {
+  description = "Git repository org/user contains for addons"
+  type        = string
+  default     = "https://github.com/Azure-Samples"
+}
+variable "gitops_addons_repo" {
+  description = "Git repository contains for addons"
+  type        = string
+  default     = "aks-platform-engineering"
+}
+variable "gitops_addons_revision" {
+  description = "Git repository revision/branch/ref for addons"
+  type        = string
+  default     = "main"
+}
+variable "gitops_addons_basepath" {
+  description = "Git repository base path for addons"
+  type        = string
+  default     = "gitops/"
+}
+variable "gitops_addons_path" {
+  description = "Git repository path for addons"
+  type        = string
+  default     = "bootstrap/control-plane/addons"
+}
+
+# Workloads Git
+variable "gitops_workload_org" {
+  description = "Git repository org/user contains for workload"
+  type        = string
+  default     = "https://github.com/Azure-Samples"
+}
+variable "gitops_workload_repo" {
+  description = "Git repository contains for workload"
+  type        = string
+  default     = "aks-platform-engineering"
+}
+variable "gitops_workload_revision" {
+  description = "Git repository revision/branch/ref for workload"
+  type        = string
+  default     = "main"
+}
+variable "gitops_workload_basepath" {
+  description = "Git repository base path for workload"
+  type        = string
+  default     = "gitops/"
+}
+variable "gitops_workload_path" {
+  description = "Git repository path for workload"
+  type        = string
+  default     = "apps"
+}
