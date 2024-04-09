e22b64e53ec57025017503ff3f1c6635df409178
Saverio Proto <saverioproto@microsoft.com>
2024-04-09 16:10:21 +0200

    Simplify capz identity resources count conditions

--- terraform/identity.tf
+++ terraform/identity.tf
@@ -1,19 +1,19 @@
 resource "azurerm_user_assigned_identity" "capz" {
-  count               = try(var.addons.enable_cluster_api_operator, false) ? 1 : 0
+  count               = var.infrastructure_provider == "capz" ? 1 : 0
   name                = "capz"
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
 }
 
 resource "azurerm_role_assignment" "capz_role_assignment" {
-  count                = try(var.addons.enable_cluster_api_operator, false) ? 1 : 0
+  count                = var.infrastructure_provider == "capz" ? 1 : 0
   scope                = azurerm_resource_group.this.id
   role_definition_name = "Owner"
   principal_id         = azurerm_user_assigned_identity.capz[0].principal_id
 }
 
 resource "azurerm_federated_identity_credential" "capz" {
-  count               = try(var.addons.enable_cluster_api_operator, false) ? 1 : 0
+  count               = var.infrastructure_provider == "capz" ? 1 : 0
   depends_on          = [module.aks]
   name                = "capz-manager-credential"
   resource_group_name = azurerm_resource_group.this.name
@@ -24,7 +24,7 @@ resource "azurerm_federated_identity_credential" "capz" {
 }
 
 resource "azurerm_federated_identity_credential" "service_operator" {
-  count               = try(var.addons.enable_cluster_api_operator, false) ? 1 : 0
+  count               = var.infrastructure_provider == "capz" ? 1 : 0
   depends_on          = [module.aks]
   name                = "serviceoperator"
   resource_group_name = azurerm_resource_group.this.name
